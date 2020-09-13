587037f384e79d586d5bfb569bc8aa267fc26b20
firebfm <firebfm@gmail.com>
2020-09-13 14:26:21 -0400

    profileid image

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const database = {
 			joined: new Date()
 		},
 		{
-		id: '124',
-		name: 'Sally',
-		email: 'sally@gmail.com',
-		password: 'apples',
-		entries: 0,
-		joined: new Date()
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'apples',
+			entries: 0,
+			joined: new Date()
 		}
 	]
 }
@@ -48,6 +48,35 @@ app.post('/register', (req, res) => {
 			joined: new Date()
 	})
 	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++
+			return res.json(user.entries);
+		}
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
 })
 
 app.listen(3000, ()=> {
