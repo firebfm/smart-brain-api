4710b9edb01a91c8b1891b289780ecc0a8ff22b0
firebfm <firebfm@gmail.com>
2020-09-14 14:49:32 -0400

    postgres register
    front end unchanged;

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -42,14 +57,17 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	database.users.push({
-			id: '125',
-			name: name,
+	db('users')
+		.returning('*')
+		.insert({
 			email: email,
-			entries: 0,
+			name: name,
 			joined: new Date()
-	})
-	res.json(database.users[database.users.length-1])
+		})
+		.then(user => {
+			res.json(user[0]);
+		})
+		.catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
