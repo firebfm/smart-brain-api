a243f5f83ec45cb24321b2ef28abcf615e35b787
firebfm <firebfm@gmail.com>
2020-09-14 15:14:11 -0400

    get user profile;

--- server.js
+++ server.js
@@ -72,14 +72,15 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
 	db.select('*').from('users').where({id})
 		.then(user => {
-		console.log(user[0]);
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
+			if (user.length) {
+				res.json(user[0])
+			} else {
+				res.status(400).json('Not found')
+			}
+		})
+		.catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
